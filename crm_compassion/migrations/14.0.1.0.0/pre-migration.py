448a0c5013ccef304607c1fc049206d052b048de
Emanuel Cino <ecino@compassion.ch>
2024-01-30 08:28:55 +0100

    T0718 FIX crm_compassion migration

--- crm_compassion/migrations/14.0.1.0.0/pre-migration.py
+++ crm_compassion/migrations/14.0.1.0.0/pre-migration.py
@@ -4,5 +4,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_fields(
-        env [("recurring.contract", "recurring_contract", "user_id", "ambassador_id")]
+        env, [("recurring.contract", "recurring_contract", "user_id", "ambassador_id")]
     )
