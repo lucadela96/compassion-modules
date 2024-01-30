0506c3f016868cce6133196485976bab3e257c90
Emanuel Cino <ecino@compassion.ch>
2024-01-30 11:20:55 +0100

    T0718 FIX sponsorship_tracking migration

--- sponsorship_compassion/migrations/14.0.1.1.7/pre-migration.py
+++ sponsorship_compassion/migrations/14.0.1.1.7/pre-migration.py
@@ -10,9 +10,13 @@ def migrate(env, version):
     openupgrade.add_fields(env, [
         ("invoice_category", "account.move", "account_move", "selection", False, "sponsorship_compassion")
     ])
-    env.cr.execute("""
+    openupgrade.logged_query(env.cr, """
         UPDATE account_move m
         SET invoice_category = i.invoice_category
         FROM account_invoice i
         WHERE m.old_invoice_id = i.id AND i.invoice_category IS NOT NULL;
+    """)
+
+    openupgrade.logged_query(env.cr, """
+        UPDATE ir_model_data SET module = 'sponsorship_sub_management' WHERE module = 'sponsorship_tracking';
     """)
