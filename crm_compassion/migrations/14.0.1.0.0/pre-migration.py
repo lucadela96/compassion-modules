3375ffee4e72ccd6443314ba9d8b089be74dfaa7
Emanuel Cino <ecino@compassion.ch>
2024-01-30 08:44:24 +0100

    T0718 FIX crm_compassion migration

--- crm_compassion/migrations/14.0.1.0.0/pre-migration.py
+++ crm_compassion/migrations/14.0.1.0.0/pre-migration.py
@@ -6,3 +6,35 @@ def migrate(env, version):
     openupgrade.rename_fields(
         env, [("recurring.contract", "recurring_contract", "user_id", "ambassador_id")]
     )
+    openupgrade.add_fields(
+        env,
+        [
+            (
+                "user_id",
+                "account.move.line",
+                "account_move_line",
+                "many2one",
+                False,
+                "crm_compassion",
+                False,
+            ),
+            (
+                "event_id",
+                "crm.event.compassion",
+                "crm_event_compassion",
+                "many2one",
+                False,
+                "crm_compassion",
+                False,
+            ),
+        ],
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move_line aml
+        SET user_id = il.user_id, event_id = il.event_id
+        FROM account_invoice_line il
+        WHERE il.id = aml.old_invoice_line_id;
+    """,
+    )
